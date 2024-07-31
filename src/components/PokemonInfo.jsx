7a8baeb1564322e4e8d84c1c5961ad7e1bdb5a98
Andrii <andrii.kondratovich@gmail.com>
2024-07-31 11:20:19 +0300

    upd: machineStatus

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -7,11 +7,18 @@ import { fetchPokemon } from "./fetchPokemon";
 
 const URL = "https://pokeapi.co/api/v2/pokemon";
 
+const machineStatus = {
+  IDLE: "idle",
+  PENDING: "pending",
+  RESOLVED: "resolved",
+  REJECTED: "rejected",
+};
+
 class PokemonInfo extends Component {
   state = {
     pokemon: null,
     error: null,
-    status: "idle",
+    status: machineStatus.IDLE,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -19,15 +26,15 @@ class PokemonInfo extends Component {
     const prevPokemonName = prevProps.pokemonName;
 
     if (prevPokemonName !== nextPokemonName) {
-      this.setState({ status: "pending" });
+      this.setState({ status: machineStatus.PENDING });
 
       fetchPokemon(nextPokemonName, URL)
         .then(pokemon => {
-          this.setState({ pokemon, status: "resolved" });
+          this.setState({ pokemon, status: machineStatus.RESOLVED });
           toast.success(`${pokemon.name} was found`);
         })
         .catch(error => {
-          this.setState({ error, status: "rejected" });
+          this.setState({ error, status: machineStatus.REJECTED });
           toast.error(error.message);
         });
     }
@@ -37,19 +44,19 @@ class PokemonInfo extends Component {
     const { error, pokemon, status } = this.state;
     const { pokemonName } = this.props;
 
-    if (status === "idle") {
+    if (status === machineStatus.IDLE) {
       return <div>Введіть ім'я покемона</div>;
     }
 
-    if (status === "pending") {
+    if (status === machineStatus.PENDING) {
       return <PokemonPendingView pokemonName={pokemonName} />;
     }
 
-    if (status === "rejected") {
+    if (status === machineStatus.REJECTED) {
       return <PokemonErrorView message={error.message} />;
     }
 
-    if (status === "resolved") {
+    if (status === machineStatus.RESOLVED) {
       return <PokemonDataView pokemon={pokemon} />;
     }
   }
