41d18e75eccc599b0a7938302ea8de0d681c617d
Andrii <andrii.kondratovich@gmail.com>
2024-07-31 11:07:11 +0300

    upd: to async fn

--- src/components/fetchPokemon.js
+++ src/components/fetchPokemon.js
@@ -1,11 +1,12 @@
-function fetchPokemon(name, url) {
-  return fetch(`${url}/${name}`).then(res => {
-    if (res.ok) {
-      return res.json();
-    }
+async function fetchPokemon(name, url) {
+  const res = await fetch(`${url}/${name}`);
+  if (res.ok) {
+    return res.json();
+  }
 
-    return Promise.reject(new Error(`Покемона з ім'ям ${name} не знайдено`));
-  });
+  return await Promise.reject(
+    new Error(`Покемона з ім'ям ${name} не знайдено`),
+  );
 }
 
 export { fetchPokemon };
